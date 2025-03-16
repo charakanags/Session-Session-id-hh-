fa924e7219235d35c9d5e98bfc9502126624347a
charakanags <repomax074@gmail.com>
2025-03-16 12:36:45 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || "session id",
+SESSION_ID: process.env.SESSION_ID || "Golden-Queen-MD-58hBUQLS#YRRzCsuJFaJtRwN_gpXMPZ_0YVW76cw_PniI1SXzWNQftrc",
 PREFIX: process.env.PREFIX || '.' ,
 MODE: process.env.MODE === undefined ?"public" : process.env.MODE,
 AUTO_VOICE: process.env.AUTO_VOICE === undefined ? "false" : process.env.AUTO_VOICE,
